85ce28cf3a6be002c24f59a9e6ae423a37bb1a40
Scott M. Barbour <smbarbour@gmail.com>
2015-02-15 22:25:52 -0600

    Define initial command-line args

--- src/main/java/org/mcupdater/QuickServer.java
+++ src/main/java/org/mcupdater/QuickServer.java
@@ -1,11 +1,19 @@
 package org.mcupdater;
 
+import joptsimple.ArgumentAcceptingOptionSpec;
+import joptsimple.OptionParser;
+
+import java.io.File;
+
 /**
  * Created by sbarbour on 2/13/15.
  */
 public class QuickServer {
 
     public static void main(String[] args){
-
+        OptionParser optParser = new OptionParser();
+        optParser.allowsUnrecognizedOptions();
+        ArgumentAcceptingOptionSpec<String> packSpec = optParser.accepts("ServerPack").withRequiredArg().ofType(String.class);
+        ArgumentAcceptingOptionSpec<File> rootSpec = optParser.accepts("MCURoot").withRequiredArg().ofType(File.class);
     }
 }
